d4f4fa8bf781153e9143c2e65ca383409ca7c381
BrenoJLeal <140741784+BrenoJLeal@users.noreply.github.com>
2024-01-25 21:53:17 -0300

    Update BlogDetails.js

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -8,7 +8,7 @@ const BlogDetails = () => {
     const history = useHistory();
 
     const handleClick = () =>{
-        fetch('https://json-server-ashen-rho.vercel.app/blogs' + blog.id,{
+        fetch('http://localhost:8000/blogs/' + blog.id,{
             method: 'DELETE'
         }).then(() =>{
             history.push('/')
